ecf414bdfee4bc951ee2fcec09b0f3e18de42172
Kenzie Treese <mtreese@stowers.org>
2025-04-07 15:26:25 -0500

    updated file names and modified script

--- Predict.py
+++ Predict.py
@@ -2,6 +2,7 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
+from tensorflow.keras import backend as K
 import matplotlib.pyplot as plt
 import pyreadr
 import os
@@ -25,7 +26,7 @@ mean = np.load('meanAdult.npy')
 
 # Import target dataset (see Readme.txt for requirements). These lines are required if you are importing the RDS file. Use the function "pyreadr.read_r" to read the file.
 print(f"Loading target dataset...")
-result = pyreadr.read_r('target_expressionMatrix.rds')
+result = pyreadr.read_r('../target_expressionMatrix.rds')
 Target = result[None]
 Target = Target.T
 print(f"Target dataset shape: {Target.shape}")
@@ -47,61 +48,61 @@ print(f"Encoder classes: {encoder.classes_}")
 
 
 # Function to make prediction
-def predict_with_confidence(model, x, no_classes, n_iter=500):
-    prediction = model.predict(x)
-    softmax = np.amax(prediction, axis=1)
-    classes = np.argmax(prediction, axis=1)
-
-    f = K.function([model.layers[0].input, K.learning_phase()],
-               [model.layers[-1].output])
-
-    bootstrap = np.zeros((n_iter,) + (x.shape[0], no_classes))
-    for i in range(n_iter):
-        bootstrap[i, :, :] = f((x, 1))[0]
-
-    BSmax = np.argmax(bootstrap, axis=2)
-    confidence = np.sum(BSmax == classes, axis=0) / BSmax.shape[0]
-    return classes, softmax, confidence
-
-
-# If you are classifying a large dataset on a machine with limited memory, use this alternative function instead
 # def predict_with_confidence(model, x, no_classes, n_iter=500):
 #     prediction = model.predict(x)
 #     softmax = np.amax(prediction, axis=1)
 #     classes = np.argmax(prediction, axis=1)
-    
-#     @tf.function
-#     def f(x):
-#         return model(x, training=True)
-    
-#     bootstrap = np.zeros((n_iter, x.shape[0], no_classes))
+
+#     f = K.function([model.layers[0].input, K.learning_phase()],
+#                [model.layers[-1].output])
+
+#     bootstrap = np.zeros((n_iter,) + (x.shape[0], no_classes))
 #     for i in range(n_iter):
-#         bootstrap[i, :, :] = f(x).numpy()
-#     BSmax1 = np.argmax(bootstrap, axis=2)    
-    
-#     bootstrap = np.zeros((n_iter, x.shape[0], no_classes))
-#     for i in range(n_iter):
-#         bootstrap[i, :, :] = f(x).numpy()
-#     BSmax2 = np.argmax(bootstrap, axis=2) 
-    
-#     bootstrap = np.zeros((n_iter, x.shape[0], no_classes))
-#     for i in range(n_iter):
-#         bootstrap[i, :, :] = f(x).numpy()
-#     BSmax3 = np.argmax(bootstrap, axis=2) 
-    
-#     bootstrap = np.zeros((n_iter, x.shape[0], no_classes))
-#     for i in range(n_iter):
-#         bootstrap[i, :, :] = f(x).numpy()
-#     BSmax4 = np.argmax(bootstrap, axis=2) 
-    
-#     bootstrap = np.zeros((n_iter, x.shape[0], no_classes))
-#     for i in range(n_iter):
-#         bootstrap[i, :, :] = f(x).numpy()
-#     BSmax5 = np.argmax(bootstrap, axis=2) 
-    
-#     BSmax = np.concatenate((BSmax1, BSmax2, BSmax3, BSmax4, BSmax5), axis=0)
+#         bootstrap[i, :, :] = f((x, 1))[0]
+
+#     BSmax = np.argmax(bootstrap, axis=2)
 #     confidence = np.sum(BSmax == classes, axis=0) / BSmax.shape[0]
 #     return classes, softmax, confidence
+
+
+# If you are classifying a large dataset on a machine with limited memory, use this alternative function instead
+def predict_with_confidence(model, x, no_classes, n_iter=500):
+    prediction = model.predict(x)
+    softmax = np.amax(prediction, axis=1)
+    classes = np.argmax(prediction, axis=1)
+    
+    @tf.function
+    def f(x):
+        return model(x, training=True)
+    
+    bootstrap = np.zeros((n_iter, x.shape[0], no_classes))
+    for i in range(n_iter):
+        bootstrap[i, :, :] = f(x).numpy()
+    BSmax1 = np.argmax(bootstrap, axis=2)    
+    
+    bootstrap = np.zeros((n_iter, x.shape[0], no_classes))
+    for i in range(n_iter):
+        bootstrap[i, :, :] = f(x).numpy()
+    BSmax2 = np.argmax(bootstrap, axis=2) 
+    
+    bootstrap = np.zeros((n_iter, x.shape[0], no_classes))
+    for i in range(n_iter):
+        bootstrap[i, :, :] = f(x).numpy()
+    BSmax3 = np.argmax(bootstrap, axis=2) 
+    
+    bootstrap = np.zeros((n_iter, x.shape[0], no_classes))
+    for i in range(n_iter):
+        bootstrap[i, :, :] = f(x).numpy()
+    BSmax4 = np.argmax(bootstrap, axis=2) 
+    
+    bootstrap = np.zeros((n_iter, x.shape[0], no_classes))
+    for i in range(n_iter):
+        bootstrap[i, :, :] = f(x).numpy()
+    BSmax5 = np.argmax(bootstrap, axis=2) 
+    
+    BSmax = np.concatenate((BSmax1, BSmax2, BSmax3, BSmax4, BSmax5), axis=0)
+    confidence = np.sum(BSmax == classes, axis=0) / BSmax.shape[0]
+    return classes, softmax, confidence
 
 
 # Predict using the new dataset
